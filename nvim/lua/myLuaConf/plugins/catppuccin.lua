4ca1bdfb8e58ce9ec905dcaff8a4554f2a8277a1
Liam Berry <liamandberry@gmail.com>
2024-11-27 10:02:31 +0200

    moved catppucin

---
+++ nvim/lua/myLuaConf/plugins/catppuccin.lua
@@ -0,0 +1,42 @@
+return {
+ {
+    "catppuccin-nvim",
+    for_cat = 'general.telescope',
+    lazy = false,
+    after = function(plugin)
+      require("catppuccin").setup({
+        compile_path = nixCats("cacheDir"),
+        -- "rosewater" | "flamingo" | "pink" | "mauve" | "red" | "maroon" | "peach" | "yellow" | "green" | "teal" | "sky" | "sapphire" | "blue" | "lavender" | "text" | "subtext1" | "subtext0" | "overlay2" | "overlay1" | "overlay0" | "surface2" | "surface1" | "surface0" | "base" | "mantle" | "crust"
+        custom_highlights = function(colors)
+          return {
+            LineNr = { fg = colors.teal },
+
+          }
+        end,
+        flavour = "mocha",
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          treesitter = true,
+          noice = true,
+          notify = true,
+          dap = true,
+          fzf = true,
+          flash = true,
+          dap_ui = true,
+          fidget = true,
+          markdown = true,
+          which_key = true,
+          rainbow_delimiters = true,
+          nvim_surround = true,
+          dropbar = {
+            enabled = true,
+            color_mode = true, -- enable color for kind's texts, not just kind's icons
+          },
+        },
+      })
+    end,
+  },
+}
