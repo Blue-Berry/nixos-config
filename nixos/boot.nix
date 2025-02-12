d27bb1874ebc9d066f40b10425412129a8726cbf
liam <liamandberry@gmail.com>
2025-02-12 22:15:54 +0200

    updated kernel

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -1,5 +1,6 @@
-{...}: {
+{pkgs,...}: {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelModules = ["uinput"];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 }
