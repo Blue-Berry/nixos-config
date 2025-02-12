fad4649cd3ba60eda68d029e5041413ddb853016
liam <liamandberry@gmail.com>
2025-02-12 23:30:09 +0200

    update

--- nixos/apps/ollama.nix
+++ nixos/apps/ollama.nix
@@ -1,8 +1,14 @@
-{...} : {
+{...}: {
   services.ollama = {
-  enable = true;
-  acceleration = "rocm";
-  # Optional: load models on startup
-  loadModels = [];
-};
-        }
+    enable = true;
+    acceleration = "rocm";
+    # Optional: load models on startup
+    loadModels = [];
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1030"; # used to be necessary, but doesn't seem to anymore
+    };
+    rocmOverrideGfx = "10.3.0";
+  };
+
+  services.open-webui.enable = true;
+}
--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -1,8 +1,10 @@
 {pkgs, ...}: {
-  fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.droid-sans-mono
-    atkinson-monolegible
-    atkinson-hyperlegible
-  ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+  fonts.packages = with pkgs;
+    [
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
+      atkinson-monolegible
+      atkinson-hyperlegible
+    ]
+    ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
 }
