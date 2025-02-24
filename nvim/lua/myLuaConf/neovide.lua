3631a2f09b00168817759721d03db7fd8746bffe
Liam Berry <liamandberry@gmail.com>
2025-02-24 11:50:44 +0200

    neovide pasting

--- nvim/lua/myLuaConf/neovide.lua
+++ nvim/lua/myLuaConf/neovide.lua
@@ -1,4 +1,7 @@
 if vim.g.neovide then
     vim.o.guifont = "FiraCode Nerd Font:h12"
     vim.g.neovide_scale_factor = 0.8
+    vim.api.nvim_set_keymap("v", "<sc-c>", '"+y', { noremap = true }) -- Select line(s) in visual mode and copy (CTRL+Shift+V)
+    vim.api.nvim_set_keymap("i", "<sc-v>", '<ESC>"+p', { noremap = true }) -- Paste in insert mode (CTRL+Shift+C)
+    vim.api.nvim_set_keymap("n", "<sc-v>", '"+p', { noremap = true }) -- Paste in normal mode (CTRL+Shift+C)
 end
