fc3693fef95e2e86188e0c4a5f0dc64ad0c93d98
liam <liamandberry@gmail.com>
2025-03-02 19:55:06 +0200

    working alloc scan

--- nvim/after/plugin/alloc_scan.lua
+++ nvim/after/plugin/alloc_scan.lua
@@ -1,12 +1,20 @@
 -- This plugin provides a :SayHello command to say hello.
 
-local path_to_exe = vim.fn.expand("<sfile>:h").."/bin/main.exe"
+local path_to_exe = vim.fn.expand("<sfile>:h") .. "/bin/main.exe"
 
 vim.api.nvim_create_user_command(
   "AllocScan",
   function(args)
     local job = vim.fn.jobstart({ path_to_exe }, { rpc = true })
-    vim.rpcrequest(job, "allocScan", args.args)
+    vim.rpcrequest(job, "allocScan")
+  end,
+  { bar = true, nargs = 0 })
+
+vim.api.nvim_create_user_command(
+  "AllocScanFile",
+  function(args)
+    local job = vim.fn.jobstart({ path_to_exe }, { rpc = true })
+    vim.rpcrequest(job, "allocScanFile", args.args)
   end,
   { bar = true, nargs = 1, complete = "user" })
 
@@ -16,4 +24,4 @@ vim.api.nvim_create_user_command(
     local job = vim.fn.jobstart({ path_to_exe }, { rpc = true })
     vim.rpcrequest(job, "allocClear")
   end,
-  { bar = true, nargs = 0})
+  { bar = true, nargs = 0 })
