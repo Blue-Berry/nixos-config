2f343a66c380365e71d79c7a3b09cd3bdbee11cb
Liam Berry <liamandberry@gmail.com>
2025-03-04 14:10:23 +0200

    added desc

--- nvim/after/ftplugin/ocaml.lua
+++ nvim/after/ftplugin/ocaml.lua
@@ -2,5 +2,5 @@ local set = vim.opt_local
 
 set.shiftwidth = 2
 
-vim.keymap.set("n", "<space>cop", require("ocaml.mappings").dune_promote_file, { buffer = 0 })
-vim.keymap.set("n", "<space>cod", require("ocaml.mappings").destruct, { buffer = 0 })
+vim.keymap.set("n", "<space>cop", require("ocaml.mappings").dune_promote_file, { buffer = 0, desc = "Promote file" })
+vim.keymap.set("n", "<space>cod", require("ocaml.mappings").destruct, { buffer = 0, desc = "Deconstruct match" })
