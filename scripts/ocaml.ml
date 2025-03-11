8c468fa46bc72e3cb0fae8258fc40166f44b7dae
liam <liamandberry@gmail.com>
2025-03-12 00:30:46 +0200

    example scripts

---
+++ scripts/ocaml.ml
@@ -0,0 +1,9 @@
+#!/usr/bin/env nix-shell
+(*
+#!nix-shell --pure -i utop -p ocaml ocamlPackages.findlib ocamlPackages.core ocamlPackages.utop
+*)
+
+#use "topfind";;
+#require "core";;
+
+Core.print_string "Hello world! 🚀 \n";;
