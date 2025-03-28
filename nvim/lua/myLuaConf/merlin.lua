9e1d1bdcfb053e13f780ff1493d2ccce42ff498c
Liam Berry <liamandberry@gmail.com>
2025-03-28 20:25:49 +0200

    merlin helptags

--- nvim/lua/myLuaConf/merlin.lua
+++ nvim/lua/myLuaConf/merlin.lua
@@ -1,4 +1,10 @@
 
 local merlinPath = nixCats('merlinPath').."/share/merlin/vim"
 vim.api.nvim_command(string.format('execute "set rtp+=%s"', merlinPath))
--- vim.api.nvim_command(string.format('helptags %s/share/merlin/vim/doc', merlinPath))
+
+local dest_dir = "/tmp/merlin/vim/doc"
+vim.fn.mkdir(dest_dir, 'p')
+local dest_path = dest_dir .. '/' .. "merlin.txt"
+local source_path = merlinPath .. '/doc/merlin.txt'
+vim.fn.system { 'cp', source_path, dest_path }
+vim.api.nvim_command(string.format('execute "helptags %s"', dest_dir))
