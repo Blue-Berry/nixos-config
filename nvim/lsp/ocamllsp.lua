797a2263666a2fe98ad3c5d2b66d76350cd33879
Liam Berry <liamandberry@gmail.com>
2025-04-07 10:37:42 +0200

    disable custom codelens

--- nvim/lsp/ocamllsp.lua
+++ nvim/lsp/ocamllsp.lua
@@ -1,33 +1,33 @@
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(args)
-    local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-    if client:supports_method('textDocument/codeLens') then
-      -- code lens
-      local codelens = vim.api.nvim_create_augroup("LSPCodeLens", { clear = true })
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "CursorHold" }, {
-        group = codelens,
-        callback = function()
-          -- set the highlight for the code lens and inlay hints
-          -- :h treesitter-highlight-groups
-          -- vim.api.nvim_set_hl(0, "Comment", { link = "@type", italic = true }) -- inlay hints
-          -- vim.api.nvim_set_hl(0, "VirtNonText", { link = "@attribute" }) -- code lens
-          local colors = require("catppuccin.palettes").get_palette "macchiato"
-          vim.api.nvim_set_hl(0, "VirtNonText", { fg = colors.peach, italic = true }) -- code lens
-          vim.lsp.codelens.refresh()
-          require("conf.lsp.codelens").refresh_virtlines()
-        end,
-        buffer = args.buf,
-      })
-
-      vim.keymap.set(
-        "n",
-        "<leader>tt",
-        require("conf.lsp.codelens").toggle_virtlines,
-        { silent = true, desc = "Toggle code lens", buffer = 0 }
-      )
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd('LspAttach', {
+--   callback = function(args)
+--     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+--     if client:supports_method('textDocument/codeLens') then
+--       -- code lens
+--       local codelens = vim.api.nvim_create_augroup("LSPCodeLens", { clear = true })
+--       vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "CursorHold" }, {
+--         group = codelens,
+--         callback = function()
+--           -- set the highlight for the code lens and inlay hints
+--           -- :h treesitter-highlight-groups
+--           -- vim.api.nvim_set_hl(0, "Comment", { link = "@type", italic = true }) -- inlay hints
+--           -- vim.api.nvim_set_hl(0, "VirtNonText", { link = "@attribute" }) -- code lens
+--           local colors = require("catppuccin.palettes").get_palette "macchiato"
+--           vim.api.nvim_set_hl(0, "VirtNonText", { fg = colors.peach, italic = true }) -- code lens
+--           vim.lsp.codelens.refresh()
+--           require("conf.lsp.codelens").refresh_virtlines()
+--         end,
+--         buffer = args.buf,
+--       })
+--
+--       vim.keymap.set(
+--         "n",
+--         "<leader>tt",
+--         require("conf.lsp.codelens").toggle_virtlines,
+--         { silent = true, desc = "Toggle code lens", buffer = 0 }
+--       )
+--     end
+--   end,
+-- })
 
 return {
   cmd = { 'ocamllsp' },
