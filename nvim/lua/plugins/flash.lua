ce2c5eb32b5d2862848d83822268129d467d8fdb
liam <liamandberry@gmail.com>
2025-04-09 22:55:47 +0200

    change flash

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -4,7 +4,7 @@ return {
     for_cat = 'general.always',
     event = "DeferredUIEnter",
     keys = {
-      { "<leader>s", function() require("flash").jump() end, mode = { "n", "x", "o" },                            desc = "Flash" },
+      { "zk", function() require("flash").jump() end, mode = { "n", "x", "o" },                            desc = "Flash" },
       { "R",         mode = { "o", "x" },                    function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     },
     after = function(plugin)
