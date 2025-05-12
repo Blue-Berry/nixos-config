bc26a1caa0b00b94121834edb62a38f1d291ee43
Liam Berry <liamandberry@gmail.com>
2025-05-12 08:21:41 +0200

    added aarch64 emulation to boot

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -2,5 +2,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelModules = ["uinput"];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   # boot.kernelPackages = pkgs.linuxPackages_latest;
 }
--- profiles/work/configuration.nix
+++ profiles/work/configuration.nix
@@ -106,7 +106,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = false;
   boot.extraModulePackages = [config.boot.kernelPackages.rtl8814au];
-  # boot.binfmt.emulatedSystems = ["aarch64-linux"];
+  boot.binfmt.emulatedSystems = ["aarch64-linux"];
 
   networking.hostName = "liam-nixos";
   networking.networkmanager.enable = true;
