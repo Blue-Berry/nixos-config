395115029744da22fd07c9f83c547b1dd08b5d1a
liam <liamandberry@gmail.com>
2025-05-18 12:39:37 +0200

    update

--- nixos/apps/steam.nix
+++ nixos/apps/steam.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
@@ -6,4 +6,15 @@
   };
 
   programs.gamemode.enable = true;
+
+  systemd.user.services.steam = {
+    enable = true;
+    description = "Open Steam in the background at boot";
+    serviceConfig = {
+      ExecStart = "${pkgs.steam}/bin/steam -nochatui -nofriendsui -silent %U";
+      wantedBy = ["graphical-session.target"];
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+  };
 }
