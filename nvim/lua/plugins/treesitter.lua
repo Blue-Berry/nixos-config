c21ba230478eb9788a213f28078d337b55069b2c
Liam Berry <liamandberry@gmail.com>
2025-06-03 17:41:34 +0200

    incremental selecton

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -22,10 +22,10 @@ return {
 				incremental_selection = {
 					enable = true,
 					keymaps = {
-						init_selection = "<c-space>",
-						node_incremental = "<c-space>",
+						init_selection = "<leader>]",
+						node_incremental = "]",
 						scope_incremental = "<c-s>",
-						node_decremental = "<M-space>",
+						node_decremental = "[",
 					},
 				},
 				textobjects = {
