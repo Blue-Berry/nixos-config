ea6ff80ff1be7f8d3c3b415248650473741b9457
Liam Berry <liamandberry@gmail.com>
2025-06-06 08:11:49 +0200

    zsh vim bindings

--- home-manager/apps/zsh.nix
+++ home-manager/apps/zsh.nix
@@ -7,49 +7,54 @@
     starship
   ];
   # TODO: config ctrl-p to mimic up arrow, ctrl-n to mimic down arrow and ctrl-y to accept completeions
-  programs.zsh = {
-    enable = true;
-    #enableAutosuggestions = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    enableCompletion = true;
-    #enableLsColors = true;
-    shellAliases = {
-      ls = "exa --icons";
-      ll = "exa --icons -l";
-      la = "exa --icons -la";
-      tree = "exa --icons --tree --level 3";
-      lr = "exa --icons --recurse --level 3";
-    };
-    oh-my-zsh = {
+  programs = {
+    zsh = {
       enable = true;
-      plugins = [
-        "git"
-        "npm"
-        "history"
-        "node"
-        "rust"
-        "deno"
-        "vi-mode"
-      ];
-      theme = "";
+      #enableAutosuggestions = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+      #enableLsColors = true;
+      shellAliases = {
+        ls = "exa --icons";
+        ll = "exa --icons -l";
+        la = "exa --icons -la";
+        tree = "exa --icons --tree --level 3";
+        lr = "exa --icons --recurse --level 3";
+      };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "npm"
+          "history"
+          "node"
+          "rust"
+          "deno"
+          "vi-mode"
+        ];
+        theme = "";
+      };
+      # for yazi
+      initContent = ''
+        function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+          yazi "$@" --cwd-file="$tmp"
+          if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+              builtin cd -- "$cwd"
+          fi
+          rm -f -- "$tmp"
+        }
+
+        bindkey "^Y" autosuggest-accept
+        bindkey "^N" up-line-or-search
+        bindkey "^P" down-line-or-beginning-search
+      '';
+    };
+    # z
+    zoxide = {
+      enable = true;
+      enableZshIntegration = true;
     };
   };
-  # z
-  programs.zoxide = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  # for yazi
-  programs.zsh.initContent = ''
-    function y() {
-      local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-      yazi "$@" --cwd-file="$tmp"
-      if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-          builtin cd -- "$cwd"
-      fi
-      rm -f -- "$tmp"
-    }
-  '';
 }
