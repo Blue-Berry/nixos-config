5ba6fbf832bda1b321e8da6fdac573b6faaedb9d
Liam Berry <liamandberry@gmail.com>
2025-06-20 17:48:11 +0200

    gowin wip

--- pkgs/gowin-edu/default.nix
+++ pkgs/gowin-edu/default.nix
@@ -21,6 +21,7 @@
 , postgresql
 , unixODBC
 , pkgs
+, speechd
 }:
 
 stdenv.mkDerivation rec {
@@ -29,14 +30,44 @@ stdenv.mkDerivation rec {
 
   src = pkgs.fetchurl {
     url = "https://cdn.gowinsemi.com.cn/Gowin_V1.9.11.01_Education_Linux.tar.gz";
-    sha256 = "";
-    curlOptsList = [ "--http1.1" ];
+    sha256 = "03md5ywwjx1214b3xvv0v1bkii0ca6dq0wynw2xy9anc755r6vi3";
+    curlOptsList = [
+      "--user-agent" "Mozilla/5.0 (X11; Linux x86_64; rv:139.0) Gecko/20100101 Firefox/139.0"
+      "--header" "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+      "--header" "Accept-Language: en-US,en;q=0.5"
+      "--header" "Accept-Encoding: gzip, deflate, br, zstd"
+      "--header" "Sec-GPC: 1"
+      "--header" "Connection: keep-alive"
+      "--header" "Upgrade-Insecure-Requests: 1"
+      "--header" "Sec-Fetch-Dest: document"
+      "--header" "Sec-Fetch-Mode: navigate"
+      "--header" "Sec-Fetch-Site: cross-site"
+      "--header" "Priority: u=0, i"
+      "--header" "Pragma: no-cache"
+      "--header" "Cache-Control: no-cache"
+      "--header" "TE: trailers"
+      "--connect-timeout" "60"
+      "--max-time" "3600"
+      "--retry" "3"
+      "--retry-delay" "5"
+      "--progress-bar"
+      "--verbose"
+    ];
   };
 
   nativeBuildInputs = [
     autoPatchelfHook
     makeWrapper
     qt5.wrapQtAppsHook
+  ];
+
+  autoPatchelfIgnoreMissingDeps = [
+    "libQt5SerialBus.so.5"
+    "libQt5WebView.so.5"
+    "libQt5Designer.so.5"
+    "libQt5Quick3DAssetImport.so.5"
+    "libQt5Quick3DRender.so.5"
+    "libQt5Quick3DUtils.so.5"
   ];
 
   buildInputs = [
@@ -52,6 +83,12 @@ stdenv.mkDerivation rec {
     qt5.qtx11extras
     qt5.qtvirtualkeyboard
     qt5.qtwebengine
+    qt5.qt3d
+    qt5.qtgamepad
+    qt5.qtsensors
+    qt5.qtserialport
+    qt5.qtspeech
+    speechd
     zlib
     openssl
     libusb1
@@ -73,18 +110,19 @@ stdenv.mkDerivation rec {
   installPhase = ''
     runHook preInstall
     
-    mkdir -p $out
-    cp -r IDE/* $out/
+    mkdir -p $out/gowin-edu
+    cp -r IDE/* $out/gowin-edu/
     
     # Make binaries executable
-    chmod +x $out/bin/gw_ide
-    find $out/bin -type f -executable -exec chmod +x {} \;
+    chmod +x $out/gowin-edu/bin/gw_ide
+    find $out/gowin-edu/bin -type f -executable -exec chmod +x {} \;
     
-    # Create wrapper for main executable that prioritizes bundled Qt libraries
-    makeWrapper $out/bin/gw_ide $out/bin/gowin-edu \
-      --prefix LD_LIBRARY_PATH : "$out/lib:${lib.makeLibraryPath buildInputs}" \
+    # Create wrapper in main bin directory
+    mkdir -p $out/bin
+    makeWrapper $out/gowin-edu/bin/gw_ide $out/bin/gowin-edu \
+      --prefix LD_LIBRARY_PATH : "$out/gowin-edu/lib:${lib.makeLibraryPath buildInputs}" \
       --set QT_XKB_CONFIG_ROOT "${qt5.qtbase.dev}/share/X11/xkb" \
-      --set QT_PLUGIN_PATH "$out/plugins/qt"
+      --set QT_PLUGIN_PATH "$out/gowin-edu/plugins/qt"
     
     runHook postInstall
   '';
