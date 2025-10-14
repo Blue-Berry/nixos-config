159929a164231a1dfac152e6a8d67229a84a5c04
liam <liamandberry@gmail.com>
2025-10-14 08:44:29 +0200

    cahchix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "Your new nix config";
-
+  nixConfig = {
+    extra-substituters = [
+        "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   inputs = {
     # Nixpkgs
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
