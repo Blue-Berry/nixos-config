892ea3573c8a557ebc35026a43de04a17d505b43
Liam Berry <liamandberry@gmail.com>
2025-11-20 15:33:38 +0200

    update

--- home-manager/desktop/waybar/default.nix
+++ home-manager/desktop/waybar/default.nix
@@ -32,7 +32,7 @@
           "group/trayG"
           "battery"
           "pulseaudio"
-          # "backlight"
+          "backlight"
           "group/comms"
           "group/usage"
           "group/clockG"
@@ -66,9 +66,9 @@
           "modules" = ["network" "bluetooth"];
         };
         "backlight" = {
-          "device" = "intel_backlight";
-          "on-scroll-up" = "light -A 5";
-          "on-scroll-down" = "light -U 5";
+          # "device" = "intel_backlight";
+          # "on-scroll-up" = "light -A 5";
+          # "on-scroll-down" = "light -U 5";
           "format" = "{icon} {percent}%";
           "format-icons" = ["󰃝" "󰃞" "󰃟" "󰃠"];
         };
