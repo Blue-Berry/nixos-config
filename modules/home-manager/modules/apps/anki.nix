3dee1a90592e31b7be720e2a4c278aa4662de574
Liam Berry <liamandberry@gmail.com>
2026-01-12 11:11:28 +0200

    anki onigiri

--- modules/home-manager/modules/apps/anki.nix
+++ modules/home-manager/modules/apps/anki.nix
@@ -1,10 +1,13 @@
 {
   lib,
   config,
+  pkgs,
   ...
-}: let
+}:
+let
   cfg = config.homeModules.apps.anki;
-in {
+in
+{
   options.homeModules.apps.anki = {
     enable = lib.mkOption {
       type = lib.types.bool;
@@ -32,16 +35,29 @@ in {
   config = lib.mkIf cfg.enable {
     programs.anki = {
       enable = true;
-      theme =
-        if cfg.darkMode
-        then "dark"
-        else "light";
+      theme = if cfg.darkMode then "dark" else "light";
       sync = {
         autoSync = true;
         username = cfg.username;
         syncMedia = true;
         keyFile = cfg.keyFile;
       };
+      addons = [
+        pkgs.ankiAddons.review-heatmap
+        pkgs.ankiAddons.passfail2
+        # pkgs.ankiAddons.recolor
+        (pkgs.anki-utils.buildAnkiAddon (finalAttrs: {
+          pname = "onigiri";
+          version = "1.0.7.1";
+          src = pkgs.fetchFromGitHub {
+            owner = "thepeacemonk";
+            repo = "Onigiri";
+            rev = "v.${finalAttrs.version}-beta";
+            hash = "sha256-VVBZk+AUKk90+weFiHqGqP3uzXUP+j+lc/0HWLO8uco=";
+          };
+          sourceRoot = "${finalAttrs.src.name}";
+        }))
+      ];
     };
     stylix.targets.anki = {
       enable = true;
