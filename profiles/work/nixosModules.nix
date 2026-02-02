98f1bd96047834cd61ef4bfc46a96471d367b308
Liam Berry <liamandberry@gmail.com>
2026-02-02 12:01:41 +0200

    fix kanata keyboards

--- profiles/work/nixosModules.nix
+++ profiles/work/nixosModules.nix
@@ -25,8 +25,8 @@ in {
       enable = true;
       devices = [
         "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-        "/dev/input/by-path/pci-0000:05:00.3-usb-0:2.2.1:1.0-event-kbd"
-        "/dev/input/by-path/pci-0000:05:00.3-usbv2-0:2.2.1:1.0-event-kbd"
+        "/dev/input/by-path/pci-0000:05:00.3-usb-0:2.2.2:1.0-event-kbd"
+        "/dev/input/by-path/pci-0000:05:00.3-usbv2-0:2.2.2:1.0-event-kbd"
       ];
     };
     nvim = true;
