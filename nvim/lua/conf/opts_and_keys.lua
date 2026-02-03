5a2e457b68c5b12ead79e798a472da2f91a9e596
Liam Berry <liamandberry@gmail.com>
2026-02-03 15:16:37 +0200

    keybinds

--- nvim/lua/conf/opts_and_keys.lua
+++ nvim/lua/conf/opts_and_keys.lua
@@ -180,3 +180,10 @@ vim.keymap.set(
 	'"_dP',
 	{ noremap = true, silent = true, desc = "Paste over selection without erasing unnamed register" }
 )
+
+vim.keymap.set("n", "<A-x>", function()
+	require("telescope.builtin").keymaps(require("telescope.themes").get_ivy({
+		winblend = 5,
+		previewer = false,
+	}))
+end, { desc = "[/keys] execute keymaps or functions]" })
