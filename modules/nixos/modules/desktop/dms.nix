76eabdcd760e606408d5cd8918b3e68c55cbb1ae
Liam Berry <liamandberry@gmail.com>
2026-03-13 17:39:26 +0200

    dms disable nixos module

--- modules/nixos/modules/desktop/dms.nix
+++ modules/nixos/modules/desktop/dms.nix
@@ -6,9 +6,9 @@
 }: let
   cfg = config.nixosModules.desktop.dms;
 in {
-  imports = [
-    inputs.dms.nixosModules.dank-material-shell
-  ];
+  # imports = [
+  #   inputs.dms.nixosModules.dank-material-shell
+  # ];
 
   options.nixosModules.desktop.dms = {
     enable = lib.mkOption {
@@ -20,6 +20,7 @@ in {
 
   config = lib.mkIf cfg.enable {
     # Disable niri-flake's polkit agent in favor of DMS's built-in one
-    systemd.user.services.niri-flake-polkit.enable = lib.mkIf config.nixosModules.desktop.niri.enable false;
+    systemd.user.services.niri-flake-polkit.enable =
+      lib.mkIf config.nixosModules.desktop.niri.enable false;
   };
 }
