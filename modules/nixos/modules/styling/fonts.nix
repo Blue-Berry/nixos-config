74465b866e527fc3e4d8a6152c693f0ba4b76f8a
Liam Berry <liamandberry@gmail.com>
2026-03-18 12:18:26 +0200

    remove extra fonts

--- modules/nixos/modules/styling/fonts.nix
+++ modules/nixos/modules/styling/fonts.nix
@@ -14,13 +14,11 @@ in {
   };
 
   config = lib.mkIf cfg {
-    fonts.packages = with pkgs;
-      [
-        nerd-fonts.fira-code
-        nerd-fonts.droid-sans-mono
-        atkinson-monolegible
-        atkinson-hyperlegible
-      ]
-      ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+    fonts.packages = with pkgs; [
+      nerd-fonts.fira-code
+      nerd-fonts.droid-sans-mono
+      atkinson-monolegible
+      atkinson-hyperlegible
+    ];
   };
 }
